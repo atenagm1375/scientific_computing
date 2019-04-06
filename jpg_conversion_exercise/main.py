d3b347621dbd6e5f5b0b8f31517a17e229cee2e1
atenagm1375 <ashenatena@gmail.com>
2019-04-06 18:01:49 +0430

    some modifications but problem of bad output still remains

--- jpg_conversion_exercise/main.py
+++ jpg_conversion_exercise/main.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 from scipy.fftpack import dct, idct
-from constants import norm, filter, e
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+from constants import norm, filter, e, img_size
 
 
 def show_image(name, img):
@@ -12,38 +15,31 @@ def show_image(name, img):
 
 
 def normalize_image(img):
-    return np.subtract(img, norm)
+    return img - norm
 
 
 def denormalize_image(img):
-    return np.round(np.add(img, norm))
+    return img + norm
 
 
 def transform(block):
-    axes = list(range(block.ndim))
-    for ax in axes[::-1]:
-        block = dct(block, type=2, axis=ax, norm='ortho')
-    return block
-    return dct(dct(block.T, norm='ortho').T, norm='ortho')
+    return dct(dct(block, axis=0, norm="ortho"), axis=1, norm="ortho")
 
 
 def inverse_transform(block):
-    axes = list(range(block.ndim))
-    for ax in axes[::-1]:
-        block = idct(block, type=2, axis=ax, norm='ortho')
-    return block
-    return idct(idct(block.T, norm="ortho").T, norm="ortho")
+    return idct(idct(block, axis=0, norm="ortho"), axis=1, norm="ortho")
 
 
 def quantize(block):
-    return np.round(np.divide(block, e))
+    return np.round(block / e)
 
 
 def dequantize(block):
-    return np.multiply(block, e)
+    return block * e
 
 
 def to_jpg(img):
+    img = normalize_image(img)
     jpg_img = np.zeros((img.shape[0], img.shape[1]))
     for i in range(0, img.shape[0], filter[0]):
         for j in range(0, img.shape[1], filter[1]):
@@ -62,22 +58,28 @@ def from_jpg(img):
         for j in range(0, img.shape[1], filter[1]):
             iimg[i : i + filter[0], j : j + filter[1]] = \
                         dequantize(img[i : i + filter[0], j : j + filter[1]])
-            print(np.sum(iimg[i : i + filter[0], j : j + filter[1]] > 0)/8/8)
+            # print(np.sum(iimg[i : i + filter[0], j : j + filter[1]] > 0)/8/8)
             iimg[i : i + filter[0], j : j + filter[1]] = \
                         inverse_transform(iimg[i : i + filter[0], j : j + filter[1]])
             # print(iimg[i : i + filter[0], j : j + filter[1]])
 
-    return iimg
+    return denormalize_image(iimg)
 
-img = cv2.imread("test.tif", 0)
+
+Tk().withdraw()
+file_name = askopenfilename(title="Choose an image file with size {}x{}".format(img_size, img_size))
+
+img = cv2.imread(file_name, 0)
+if img.shape != (img_size, img_size):
+    raise Exception("Invalid image size")
 show_image("original image", img)
-img = normalize_image(img)
-show_image("normalized image", img)
 jpg_img = to_jpg(img)
+# print(np.min(np.min(jpg_img)), np.max(np.max(jpg_img)))
+
+jpg_img = from_jpg(jpg_img)
+cv2.imwrite("my_jpg.jpg", jpg_img)
 print(np.min(np.min(jpg_img)), np.max(np.max(jpg_img)))
-show_image("jpg image without compression", jpg_img)
-iimg = denormalize_image(from_jpg(jpg_img))
-print(np.min(np.min(iimg)), np.max(np.max(iimg)))
-show_image("inverse image", iimg)
-dif = img - iimg
+show_image("jpg image", jpg_img)
+
+dif = img - jpg_img
 show_image("dif", dif)
