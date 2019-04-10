182087148d1b70740c271913c4580d68efd847ee
atenagm1375 <ashenatena@gmail.com>
2019-04-10 10:38:33 +0430

    add plot

--- DFT_exercise/main.py
+++ DFT_exercise/main.py
@@ -1,15 +1,19 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 def show_image(name, img):
+    print("Press any key to close the window(do not use x button on top of the window)")
     cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return
 
 
-img = cv2.imread("norm.jpg", 0)
+zmg = cv2.imread("norm.jpg", 0)
+img = np.zeros(zmg.shape)
+img[:zmg.shape[0], :zmg.shape[1]] = zmg
 show_image("curve", img)
 
 Z = np.fft.fft2(img)
@@ -22,14 +26,25 @@ show_image("dft", Zlog/M)
 
 M = np.max(np.max(np.abs(Z)))
 thresh_list = [0.0001, 0.001, 0.01, 0.05]
+compression_ratios = []
+distortions = []
 
 for thresh in thresh_list:
     Zthresh = np.multiply(np.abs(Z) > thresh * M, Z)
+
+    compression_ratio = np.sum(np.sum(np.abs(Zthresh) > 0)) / img.shape[0] / img.shape[1]
     print("Fraction of Fourier coefficients survived the cut:",
-            np.sum(np.sum(np.abs(Zthresh) > 0)) / img.shape[0] / img.shape[1])
+            compression_ratio)
+    compression_ratios.append(compression_ratio)
 
     zthresh = np.real(np.fft.ifft2(Zthresh))
     show_image("dft with thresholding", zthresh)
 
-    print("Distortion of compressed signal:", 100 * np.linalg.norm(img - zthresh, 'fro') ** 2 \
-            / np.linalg.norm(img, 'fro') ** 2)
+    distortion = 100 * np.linalg.norm(img - zthresh, 'fro') ** 2 / np.linalg.norm(img, 'fro') ** 2
+    print("Distortion of compressed signal:", distortion)
+    distortions.append(distortion)
+
+plt.plot(distortions, compression_ratios)
+plt.xlabel(xlabel="distortion")
+plt.ylabel(ylabel="compression ratio")
+plt.show()
